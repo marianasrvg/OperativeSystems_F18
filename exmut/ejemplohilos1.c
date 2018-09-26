5d59dfc4e848467e2d6547b73cde4268b36dc8fd
XUbuntu <sierravgmr@gmail.com>
2018-09-26 11:19:29 -0600

    alternancia estricta

---
+++ exmut/ejemplohilos1.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<pthread.h>
+
+#define NUMTHREADS 2
+
+void * tfunc(void * args);
+void tomando_cerveza(int n);
+void usando_el_wc(int n);
+
+int turno = 0;
+
+int main(){
+	pthread_t tid[NUMTHREADS];
+	int args[NUMTHREADS];
+	int i;
+	
+	srand(getpid());
+	
+	for(i = 0; i < NUMTHREADS; i++){
+		args[i] = i;
+		pthread_create(&tid[i], NULL, tfunc, &args[i]);
+	}
+	
+	for(i = 0; i < NUMTHREADS; i++){
+		pthread_join(tid[i],NULL);
+	}
+}
+
+void * tfunc(void * args){
+	int i = *((int *) args);
+	int j = 1 - i;
+	int n;
+	
+	printf("Soy el hilo %d, i=%d, j=%d\n", i,i,j);
+	
+	for(n = 0; n < 10; n++){
+		tomando_cerveza(i);
+		while(turno!=i);	//mientras no sea mi turno
+		usando_el_wc(i);
+		turno = j; 			//ceder el turno al turno
+	}
+}
+
+void tomando_cerveza(int n){
+	printf("Tomando cerveza %d\n", n);
+	usleep(rand()%3000000);
+}
+void usando_el_wc(int n){
+	printf("Entra al wc %d\n", n);
+	usleep(rand()%3000000);
+	printf("Sale del wc %d\n", n);
+}
